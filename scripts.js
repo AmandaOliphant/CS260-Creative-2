17403f610e8da69923e115b9f7ac601354ef99fd
AmandaOliphant <aeoliphant@gmail.com>
2018-10-03 00:54:18 -0600

    Added JSON functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-
 $(document).ready(function() {
+
     $("#submitCard").click(function(e) {
         e.preventDefault();
-        var value = $("#searchField").val();
+        var value = $("#cardNameField").val();
         console.log(value);
+        var image = $("#cardImage").attr("src",)
+        image.attr="http://www.wizards.com/magic/autocard.asp?name=Call of the Herd"
 
     })
 
@@ -13,5 +15,32 @@ $(document).ready(function() {
         console.log(value);
 
     })
+
+    $("#cardNameField").keyup(function() {
+        //Make and return links to cards on hover
+        var dataList = document.getElementById('card-datalist');
+        dataList.innerHTML = '';
+        var input = document.getElementById('cardNameField');
+
+        $.ajax({
+             type:'GET',
+             url: 'UST.json',
+             dataType:'json',
+             success:function(data){
+                 var cards = data.cards;
+                 $.each(cards, function(index, value) {
+                     var cardName = value.name.toUpperCase();
+                     if(cardName.startsWith($("#cardNameField").val().toUpperCase())) {
+                         var option = document.createElement('option');
+                         option.value = value.name;
+                         dataList.appendChild(option);
+                     }
+                 });
+
+             }
+         });
+
+         input.placeholder = "Search Magic cards";
+     })
 
 });
